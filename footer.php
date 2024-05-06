940b8e18070a22691cbebe80ee4d652fea6e4269
floriandelbreil <165294692+floriandelbreil@users.noreply.github.com>
2024-05-06 12:05:40 +0200

    Delete footer.php

--- footer.php
+++
@@ -1,4 +0,0 @@
-<footer>
-    <p> © 2024 Copyright : THALES ALENIA SPACE - CHECKLIST - Groupe 6</p>
-    <p> © 2024 Copyright : Université Nice Côte-d'Azur - IUT Réseaux et Télécommunications - Sophia Antipolis</p>
-</footer>
