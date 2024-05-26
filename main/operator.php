15cbc513ac061a9509e06224a6a97ea626dd7f7d
floriandelbreil <165294692+floriandelbreil@users.noreply.github.com>
2024-05-26 14:15:00 +0200

    Delete main/operator.php

--- main/operator.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    session_start();
-    require_once(__DIR__ . '/goodpractices_selection.php');
-    require_once(__DIR__ . '/goodpractices_print.php');
-    // require_once(__DIR__ . '/goodpractices_checklist.php');
-
-?>
